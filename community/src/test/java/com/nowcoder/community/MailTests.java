d7462528f03c6308cb43a99f6a97acad92322ae7
yhy-lab <463147064@qq.com>
2020-05-13 22:58:26 +0800

    mail service

---
+++ community/src/test/java/com/nowcoder/community/MailTests.java
@@ -0,0 +1,22 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.util.MailClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MailTests {
+    @Autowired
+    private MailClient mailClient;
+
+    @Test
+    public void testTestMail(){
+        mailClient.sendMail("yhy123@bupt.edu.cn","Test","Welcome.");
+    }
+}
