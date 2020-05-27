588300f4b8c33882bfb25f2911bf01381e4def90
yhy-lab <463147064@qq.com>
2020-05-27 23:50:04 +0800

    显示登陆信息

---
+++ community/src/main/java/com/nowcoder/community/util/HostHoler.java
@@ -0,0 +1,24 @@
+package com.nowcoder.community.util;
+
+import com.nowcoder.community.entity.User;
+import org.springframework.stereotype.Component;
+
+/**
+ * 持有用户的信息，用于代替session对象
+ */
+@Component
+public class HostHoler {
+
+    private ThreadLocal<User> users = new ThreadLocal<>();
+
+    public void setUser(User user){
+        users.set(user);
+    }
+    public User getUser(){
+        return users.get();
+    }
+
+    public void clean(){
+        users.remove();
+    }
+}
